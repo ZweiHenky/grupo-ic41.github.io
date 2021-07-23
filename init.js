0356ec0ba94f59f6220b315cf2cae1101359807e
ZweiHenky <87835882+ZweiHenky@users.noreply.github.com>
2021-07-23 03:05:26 -0500

    Create init.js

---
+++ init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyARdvZegcxXCUyMBsKmYSStkFb49rl4Bcg",
+    authDomain: "autentify41.firebaseapp.com",
+    projectId: "autentify41",
+    storageBucket: "autentify41.appspot.com",
+    messagingSenderId: "386598205961",
+    appId: "1:386598205961:web:337e642a48001a84cc16d8",
+    measurementId: "G-GNR2YLMP08"
+});
